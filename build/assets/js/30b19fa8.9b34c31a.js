7c5232a0f88e0e0e9c2dce43132739e7660988e8
hari anitha <hv8447@gmail.com>
2024-09-08 17:54:04 +0530

    Updating build files

---
+++ build/assets/js/30b19fa8.9b34c31a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_blog=self.webpackChunkmy_blog||[]).push([[6479],{1643:l=>{l.exports=JSON.parse('{"tag":{"label":"ruby","permalink":"/blog/tags/ruby","allTagsPath":"/blog/tags","count":1,"unlisted":false},"listMetadata":{"permalink":"/blog/tags/ruby","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
